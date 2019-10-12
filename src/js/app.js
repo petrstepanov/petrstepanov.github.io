549f6aeb130ac69af12eec5280c8337cb42a1c75
Petr Stepanov <petrstepanov@petrs-air.home>
2019-10-12 00:43:35 -0400

    JS errors fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 console.log("std::cout << \"Welcome to my home page!\" << std::endl;");
 console.log("std::cout << \"Shoot me an email at stepanovps@gmail.com\" << std::endl;");
 
-SplashScreen.init();
+// SplashScreen.init();
--- src/js/modules/splash-screen.js
+++ src/js/modules/splash-screen.js
@@ -4,13 +4,13 @@ var SplashScreen = (function () {
 	var DOM = {};
 	var template = [
 		'<div id="splash-screen">',
-			'<img id="splash-logo" src="./img/ps-monogram-crop.svg" class="delay-1s faster" />',
-			'<p id="splash-text" class="delay-1s faster"><small>loading</small></p>',
+		'<img id="splash-logo" src="./img/ps-monogram-crop.svg" class="delay-1s faster" />',
+		'<p id="splash-text" class="delay-1s faster"><small>loading</small></p>',
 		'</div>'
 	].join("\n");
-	
+
 	function init() {
-		DOM.$html = $('html');		
+		DOM.$html = $('html');
 		DOM.$html.append(template);
 		DOM.$splash = DOM.$html.find("#splash-screen");
 		DOM.$logo = DOM.$splash.find("#splash-logo");
@@ -18,27 +18,27 @@ var SplashScreen = (function () {
 
 		UIHelper.animateCSS(DOM.$text.get(0), 'fadeInUp');
 
-		var promiseWindowLoad = new Promise((resolve, reject) => {
+		var promiseWindowLoad = new Promise(function (resolve, reject) {
 			$(window).on("load", resolve("Stuff loaded!"));
 		});
 
-		var promiseLogoAnimate = new Promise((resolve, reject) => {
+		var promiseLogoAnimate = new Promise(function (resolve, reject) {
 			UIHelper.animateCSS(DOM.$logo.get(0), 'bounceIn', function () {
 				resolve("Logo animated!");
 			});
 		});
 
 		var that = this;
-		Promise.all([promiseWindowLoad, promiseLogoAnimate]).then((values) => {
+		Promise.all([promiseWindowLoad, promiseLogoAnimate]).then(function (values) {
 			UIHelper.animateCSS(DOM.$logo.get(0), 'bounceOut', function () {
 				DOM.$logo.css('visibility', 'hidden');
 				DOM.$text.removeClass('delay-1s');
-				UIHelper.animateCSS(DOM.$text.get(0), 'fadeOutDown', function(){
+				UIHelper.animateCSS(DOM.$text.get(0), 'fadeOutDown', function () {
 					DOM.$text.remove();
 				});
 
-				setTimeout(() => {
-					DOM.$splash.fadeOut(500, ()=>{
+				setTimeout(function () {
+					DOM.$splash.fadeOut(500, function () {
 						DOM.$splash.remove();
 					});
 				}, 500);
