cff510086f144dc18413bb3536866dc0383c0879
Petr Stepanov <stepanovps@gmail.com>
2019-10-13 12:35:47 -0400

    Implemented menu

--- src/js/modules/morph-navigation.js
+++ src/js/modules/morph-navigation.js
@@ -8,35 +8,39 @@ var MorphNavigation = (function(){
         DOM.$el = $(element);
         DOM.$morphButton = DOM.$el.find('.morph-button');
         DOM.$icon = DOM.$el.find('.js--icon');
+        DOM.$links = DOM.$el.find('li a');
     }
 
     function _bindEvents(element) {
         DOM.$morphButton.on('click', function(event){
-            if (!DOM.$el.hasClass('is-open')){
-                UIHelper.animateCSS(DOM.$icon.get(0), "fadeOut", function(){
-                    DOM.$icon.removeClass('ion-ios-menu');
-                    DOM.$icon.addClass('ion-ios-close');
-                    UIHelper.animateCSS(DOM.$icon.get(0), "fadeIn");
-                });
-                // ScrollFreezer.freeze();
-                bodyScrollLock.enableBodyScroll();
-            }
-            else {
-                UIHelper.animateCSS(DOM.$icon.get(0), "fadeOut", function(){
-                    DOM.$icon.removeClass('ion-ios-close');
-                    DOM.$icon.addClass('ion-ios-menu');
-                    UIHelper.animateCSS(DOM.$icon.get(0), "fadeIn");
-                });
-                // ScrollFreezer.release();
-                bodyScrollLock.disableBodyScroll();                
-            }
-            DOM.$el.toggleClass('is-open');             
+            // Prevent from scrolling up
+            event.preventDefault();
+            _toggleMenu();        
         });
+        DOM.$links.on('click', _toggleMenu);
     }
 
-    // function _render(){
-
-    // }
+    function _toggleMenu(){
+        if (!DOM.$el.hasClass('is-open')){
+            UIHelper.animateCSS(DOM.$icon.get(0), "fadeOut", function(){
+                DOM.$icon.removeClass('ion-ios-menu');
+                DOM.$icon.addClass('ion-ios-close');
+                UIHelper.animateCSS(DOM.$icon.get(0), "fadeIn");
+            });
+            // ScrollFreezer.freeze();
+            bodyScrollLock.disableBodyScroll();
+        }
+        else {
+            UIHelper.animateCSS(DOM.$icon.get(0), "fadeOut", function(){
+                DOM.$icon.removeClass('ion-ios-close');
+                DOM.$icon.addClass('ion-ios-menu');
+                UIHelper.animateCSS(DOM.$icon.get(0), "fadeIn");
+            });
+            bodyScrollLock.enableBodyScroll();                
+            // ScrollFreezer.release();
+        }
+        DOM.$el.toggleClass('is-open');
+    }
 
     function init(element) {
         if (element) {
