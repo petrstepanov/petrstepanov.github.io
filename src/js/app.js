8f6752fe5b4b2c8265be23473a300a584d38cc33
Petr Stepanov <stepanovps@gmail.com>
2023-12-19 17:54:04 -0800

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 var morphNavigation = require('./modules/morph-navigation');
+var bs = require('./../../node_modules/bootstrap/js/dist/modal.js');
 
 var morphNavigationElement = document.querySelector('.js--init-morph-navigation');
 morphNavigation.init(morphNavigationElement);
